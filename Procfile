2ea7aa3d68125e45913755e09f97fb0c11c32b26
v01cee <maksimagrig2@gmail.com>
2025-12-07 20:33:20 +0300

    Fix Railway config

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: npm run preview -- --host 0.0.0.0 --port $PORT
+web: npm run preview
 
--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,10 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173
   }
 })
 
